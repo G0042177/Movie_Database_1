e84db5f4eddbf2858857b2c209764cd880fe188b
G0042177 <G0042177@atu.ie>
2024-04-17 16:54:26 +0100

    testing push request 3

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -2,6 +2,6 @@ package ie.atu;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing push request!");
+        System.out.println("Testing push request 3");
     }
 }
