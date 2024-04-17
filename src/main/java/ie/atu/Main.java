28b6dc04e0957d05d1c34f9fc6175081dd633fcd
Tyrese <g00421432@atu.ie>
2024-04-17 17:09:10 +0100

    testing pull request

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -2,6 +2,6 @@ package ie.atu;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing push request 4");
+        System.out.println("Testing push request 5");
     }
 }
