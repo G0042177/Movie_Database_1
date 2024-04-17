98606cc399a3623e8748711ea7a1690f405ca316
Antoj2000 <g00385306@atu.ie>
2024-04-17 17:22:39 +0100

    Added sign up

---
+++ src/main/java/ie/atu/Sign_Up.java
@@ -0,0 +1,7 @@
+package ie.atu;
+
+public class Sign_Up extends Login_Info{
+    public Sign_Up(String name, String username, double dob, String email, String gender, String country) {
+        super(name, username, dob, email, gender, country);
+    }
+}
