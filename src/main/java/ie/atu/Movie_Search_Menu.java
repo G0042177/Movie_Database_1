1c2bee98b96f0de9f067c5efb266572c01c6510c
G0042177 <G0042177@atu.ie>
2024-04-19 16:21:38 +0100

    Added the ratings synopsis and relese date to the movie search

--- src/main/java/ie/atu/Movie_Search_Menu.java
+++ src/main/java/ie/atu/Movie_Search_Menu.java
@@ -13,18 +13,25 @@ public class Movie_Search_Menu implements Movie_Search {
     public ArrayList<String> searchMovie(Connection conn, Scanner scanner, String userSearch) throws SQLException {
         ArrayList<String> results = new ArrayList<>();
 
-        PreparedStatement stmt = conn.prepareStatement("SELECT movie.title, genre.name " +
+        PreparedStatement stmt = conn.prepareStatement("SELECT movie.title, movie.release_date, movie.rating, movie.synopsis, genre.name " +
                 "FROM movie " +
                 "JOIN genre ON movie.genre_id = genre.genre_id " +
                 "WHERE (movie.title LIKE ? OR genre.name LIKE ?)");
-        stmt.setString(1,"%"+userSearch+"%");
-        stmt.setString(2,"%"+userSearch+"%");
+        stmt.setString(1, "%" + userSearch + "%");
+        stmt.setString(2, "%" + userSearch + "%");
 
         ResultSet resultSet = stmt.executeQuery();
-        while (resultSet.next()){
+        while (resultSet.next()) {
             String movieName = resultSet.getString("title");
+            String releaseDate = resultSet.getString("release_date");
+            double rating = resultSet.getDouble("rating");
+            String synopsis = resultSet.getString("synopsis");
             String genreName = resultSet.getString("name");
-            results.add(movieName + " - "+ genreName);
+            results.add("Movie Title: " + movieName +
+                    "\nRelease Date: " + releaseDate +
+                    "\nRating: " + rating +
+                    "\nGenre: " + genreName +
+                    "\nSynopsis: " + synopsis + "\n");
         }
         return results;
     }
