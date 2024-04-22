71bc6abe17b658369d5ba6baf46d8794b206fd10
G00385306@atu.ie <g385306@atu.ie>
2024-04-22 11:57:10 +0100

    Fixed Login menu bugs, connected to db

--- src/main/java/ie/atu/Main_Menu.java
+++ src/main/java/ie/atu/Main_Menu.java
@@ -4,7 +4,7 @@ import ie.atu.pool.DatabaseUtils;
 import java.sql.*;
 public class Main_Menu {
     public void showMovies() {
-        String showMovies = "SELECT movie.movie_title, genre.name ,movie.release_date" +
+        String showMovies = "SELECT movie.title, genre.name ,movie.release_date " +
                 "FROM movie " +
                 "JOIN genre on movie.genre_id = genre.genre_id " +
                 "ORDER BY RAND() " +
@@ -15,7 +15,7 @@ public class Main_Menu {
              ResultSet resultSet = statement.executeQuery(showMovies)) {
 
             while (resultSet.next()) {
-                String movie = resultSet.getString("movie_title");
+                String movie = resultSet.getString("title");
                 String genre = resultSet.getString("name");
 
                 System.out.println(movie + " - " + genre);
@@ -25,7 +25,8 @@ public class Main_Menu {
         }
     }
     public void showGenres() {
-        String showGenres = "SELECT * FROM genre.name, FROM genre.genre_name " +
+        String showGenres = "SELECT genre.name " +
+                "FROM genre " +
                 "ORDER BY RAND() " +
                 "LIMIT 3";
 
@@ -35,7 +36,7 @@ public class Main_Menu {
 
             while (resultSet.next()) {
                 String name = resultSet.getString("name");
-                String genreName = resultSet.getString("genre_name");
+                String genreName = resultSet.getString("name");
 
                 System.out.println("Genre: " + name );
             }
